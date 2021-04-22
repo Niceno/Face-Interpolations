920eb5abc7dfe0051424255230f0b9b2f2bf4a93
Niceno <bojan.niceno.scientist@gmail.com>
2021-04-22 09:25:41 +0200

    Improved comments a bit, no biggie.
    modified:   flux_03_rc_standard_from_velocities.m

--- flux_03_rc_standard_from_velocities.m
+++ flux_03_rc_standard_from_velocities.m
@@ -6,8 +6,16 @@ function [u_if, u_af] = flux_03_rc_standard_from_velocities(  ...
                         x_c, dv, a_u,                         ...
                         u_c, p_c, p_x)
 
+  %-------------------------------------------
+  % Subtract cell-centered pressure gradients
+  %-------------------------------------------
+
   % Unit for velocity: m^3 * s / kg * kg/(m^2 s^2) = m / s
   u_if = line_avg(u_c + dv ./ spdiags(a_u, 0)' .* p_x);
+
+  %----------------------------------
+  % Add staggered pressure gradients
+  %----------------------------------
   u_if = u_if - line_avg(dv ./ spdiags(a_u, 0)') .* diff(p_c) ./ diff(x_c);
 
   u_af = [0.0,u_if,0.0];  % append boundary values (just zeroes now)
