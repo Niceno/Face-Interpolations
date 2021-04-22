430e38f0332c0ec242267f06d8fa50ebc46d74c5
Niceno <bojan.niceno.scientist@gmail.com>
2021-04-22 08:32:55 +0200

    Removed an empty line.
    modified:   flux_03_rc_standard_from_velocities.m

--- flux_03_rc_standard_from_velocities.m
+++ flux_03_rc_standard_from_velocities.m
@@ -6,8 +6,7 @@ function [u_if, u_af] = flux_03_rc_standard_from_velocities(  ...
                         x_c, dv, a_u,                         ...
                         u_c, p_c, p_x)
 
-  % Unit for velocity m^3 * s / kg * kg/(m^2 s^2) = m / s
-
+  % Unit for velocity: m^3 * s / kg * kg/(m^2 s^2) = m / s
   u_if = line_avg(u_c + dv ./ spdiags(a_u, 0)' .* p_x);
   u_if = u_if - line_avg(dv ./ spdiags(a_u, 0)') .* diff(p_c) ./ diff(x_c);
 
